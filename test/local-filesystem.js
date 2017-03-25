92f67cb116d5b2105f33944b5018dcde17e1b4f8
Alessio Coser <alessio.coser@xpeppers.com>
2017-03-25 18:56:23 +0100

    add read method to local filesystem

--- test/local-filesystem.js
+++ test/local-filesystem.js
@@ -4,39 +4,52 @@ const stringToStream = require('string-to-stream')
 const LocalFileSystem = require('..').local
 
 const folder = 'test/promise-filesystem'
-const file = 'file.txt'
-const writeFile = 'write-as-stream.txt'
+const readFileName = 'file.txt'
+const writeFileName = 'write-as-stream.txt'
 
 test('LocalFileSystem', function () {
   test.timeout('reads from Local Folder as stream', function (done) {
     var localFileSystem = new LocalFileSystem()
     var data = []
 
-    localFileSystem.readAsStream(folder, file)
+    localFileSystem.readAsStream(folder, readFileName)
     .on('data', chunk => data.push(chunk))
     .on('end', () => {
-      equal(String.prototype.concat(data), 'lorem ipsum dolor sit amet.\n')
+      equal(String.prototype.concat(data), 'Lorem ipsum dolor sit amet.\n')
       done()
     })
   }, 30000)
 
-  test.timeout('write to Local Folder as stream', function (done) {
+  test.timeout('writes to Local Folder as stream', function (done) {
     var localFileSystem = new LocalFileSystem()
     var aStream = stringToStream('Lorem ipsum dolor sot amet.\n')
 
-    localFileSystem.writeAsStream(folder, writeFile, aStream)
+    localFileSystem.writeAsStream(folder, writeFileName, aStream)
     .then((data) => {
       setTimeout(() => {
-        var writeFileContent = fs.readFileSync(`${folder}/${writeFile}`, 'utf8')
+        var fileContent = fs.readFileSync(`${folder}/${writeFileName}`, 'utf8')
 
-        deepEqual(data.Location, `${folder}/${writeFile}`)
-        deepEqual(data.FileName, writeFile)
+        deepEqual(data.Location, `${folder}/${writeFileName}`)
+        deepEqual(data.FileName, writeFileName)
         deepEqual(data.Folder, folder)
-        equal(writeFileContent, 'Lorem ipsum dolor sot amet.\n')
+        equal(fileContent, 'Lorem ipsum dolor sot amet.\n')
 
-        fs.unlinkSync(`${folder}/${writeFile}`)
+        fs.unlinkSync(`${folder}/${writeFileName}`)
         done()
       }, 1000)
+    })
+    .catch(done)
+  }, 30000)
+
+  test.timeout('reads file from Local Folder', function (done) {
+    var localFileSystem = new LocalFileSystem()
+
+    return localFileSystem.read(folder, readFileName)
+    .then(data => {
+      let fileContent = (data.Body) ? data.Body.toString() : ''
+
+      equal(fileContent, 'Lorem ipsum dolor sit amet.\n')
+      done()
     })
     .catch(done)
   }, 30000)
