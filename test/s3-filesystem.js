77c72a8d861de494161103f07246a15dea87c5e6
Alessio Coser <alessio.coser@xpeppers.com>
2017-04-06 14:30:49 +0200

    add HTTP Range Header to S3 Read method

--- test/s3-filesystem.js
+++ test/s3-filesystem.js
@@ -53,6 +53,20 @@ test('S3FileSystem', function () {
     .catch(done)
   }, 30000)
 
+  test.timeout('reads a range of file from S3 Bucket', function (done) {
+    var s3FileSystem = new S3FileSystem()
+    var HTTPRangeHeader = 'bytes=0-3'
+
+    return s3FileSystem.read(folder, inputFileName, HTTPRangeHeader)
+    .then(data => {
+      let fileContent = (data.Body) ? data.Body.toString() : ''
+
+      equal(fileContent, 'Lore')
+      done()
+    })
+    .catch(done)
+  }, 30000)
+
   test.timeout('get file head from S3 Bucket', function (done) {
     var s3FileSystem = new S3FileSystem()
 
